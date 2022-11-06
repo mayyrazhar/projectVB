c7f588f8d84c3aee9ee66e7dcb5788a6ddbd95f9
mayyrazhar <99306526+mayyrazhar@users.noreply.github.com>
2022-11-07 03:10:25 +0800

    first push

---
+++ ProjectDemoCRS/Login/LoginModel.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.SqlClient
+Public Class LoginModel
+
+    Dim conn As New SqlConnection("Data Source=C:\Users\user\Documents\VB\PROJECT\ProjectDemoCRS1.1\ProjectDemoCRS\bin\Debug\ihsanTuitionCenterDb.accdb")
+    Public Function checkLogin(user As UserClass) As Boolean
+
+        Return True
+    End Function
+End Class
